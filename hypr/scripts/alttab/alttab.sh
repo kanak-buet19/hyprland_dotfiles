da9e7a53b425d8055ff2a2a3910c46ca0cd6abcd
kanak-buet19 <rakibul.buet19@gmail.com>
2026-01-04 18:56:58 +0600

    added alt-tab setup for hyprland

---
+++ hypr/scripts/alttab/alttab.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+hyprctl -q dispatch submap alttab
+start=$1
+address=$(hyprctl -j clients | jq -r 'sort_by(.focusHistoryID) | .[] | select(.workspace.id >= 0) | select(.class != "alttab") | "\(.address)\t\(.title)"' | fzf --color prompt:green,pointer:green,current-bg:-1,current-fg:green,gutter:-1,border:bright-black,current-hl:red,hl:red \
+--cycle \
+--sync \
+--bind tab:down,shift-tab:up,start:$start,double-click:ignore \
+--wrap \
+--delimiter=$'\t' \
+--with-nth=2 \
+--preview "$XDG_CONFIG_HOME/hypr/scripts/alttab/preview.sh {}" \
+--preview-window=down:80% \
+--layout=reverse | awk -F"\t" '{print $1}')
+if [ -n "$address" ] ; then
+  echo "$address" > $XDG_RUNTIME_DIR/hypr/alttab/address
+fi
+hyprctl -q dispatch submap reset
